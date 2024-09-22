37cdfa007dde0f8ac09f476c11e3dad61b54ca97
Akash <airshula79@gmail.com>
2024-09-22 14:26:38 +0700

    first commit

--- main.py
+++ main.py
@@ -9,26 +9,45 @@ Horizontal ,vertical and Alpha integeration . These are types of image concatena
 '''
 import cv2
 
+import logging
+
 
 def main():
     img1 = cv2.imread('moon1_cropped.jpg')
     img2 = cv2.imread('monkey-nft.png')
-    v_combined_img = combine_vertically(img1, img2)
-    show_img("vertical", v_combined_img)
+    # v_combined_img = combine_vertically(img1, img2)
+    # show_img("vertical", v_combined_img)
+
+    height = img1.shape[0]
+    width = img1.shape[1]
+
+    img1 = cv2.resize(img1 , (width//2,height//2)).copy()
+    img2 = cv2.resize(img2 , (width//2,height//2)).copy()
+    beta = 0.5
+    overlay_img = cv2.addWeighted(img1, 1 - beta, img2, beta, 0)
+
+    show_img("overlayed_img",overlay_img)
+
 
 
 def combine_vertically(img1, img2):
     if img1 is None or img2 is None:
         print("unable to load images")
     else:
-        # print('height', img1.shape[0])
-        # print('width', img1.shape[1])
-        #
-        # print('height', img2.shape[0])
-        # print('width', img2.shape[1])
-        # v_combined = cv2.vconcat([img1, img2])
-        # return v_combined
-        img1 , img2 = crop_img(img1 , img2 , True)
+
+        # img1 , img2 = crop_img(img1 , img2 , False)
+
+        # img1 = cv2.resize(img1 , (img1.shape[0]//4,img2.shape[0]//2))
+        # img2 = cv2.resize(img2 , (img2.shape[0]//2,img2.shape[0]//2))
+        # h_combined = cv2.hconcat([img1,img2])
+        # return h_combined
+
+        img1= cv2.resize(img1, (img1.shape[1] // 2, img1.shape[0] // 2)).copy()
+        img2 = cv2.resize(img2, (img1.shape[1] // 2, img2.shape[0] // 2)).copy()
+        # img1, img2 = crop_img(img1, img2, True)
+
+        print(img1.shape[1] ,img2.shape[1] )
+        print(img1.shape[0] ,img2.shape[0] )
         v_combined = cv2.vconcat([img1, img2])
         return v_combined
 
@@ -37,38 +56,41 @@ def combine_horizontally(img1, img2):
     pass
 
 
+
 def crop_img(img1, img2, is_vertical=True):
     cropped_img1 = img1
     cropped_img2 = img2
     min_value = 0
 
+    img1_height = img1.shape[0]
+    img2_height = img2.shape[0]
+
+    img1_width = img1.shape[1]
+    img2_width = img2.shape[1]
+
     if not is_vertical:
-        img1_height = img1.shape[0]
-        img2_height = img2.shape[0]
 
         if img1_height > img2_height:
             min_value = img2_height
-            cropped_img1 = img1[0:0 + min_value]  # gives a range of x and y-axis coordinates to crop
+            cropped_img1 = img1[0:0 + min_value,0:0+img1_width]  # gives a range of x and y-axis coordinates to crop
         elif img2_height > img1_height:
             min_value = img1_height
-            cropped_img2 = img2[0:0 + min_value]  # gives a range of x and y-axis coordinates to crop
+            cropped_img2 = img2[0:0 + min_value , 0:0+img2_width]  # gives a range of x and y-axis coordinates to crop
 
     elif is_vertical:
-        img1_width = img1.shape[1]
-        img2_width = img2.shape[1]
 
         if img1_width > img2_width:
             min_value = img2_width
-            cropped_img1 = img1[0:0 + min_value]
+            cropped_img1 = img1[0:0+img1_height,0:0 + min_value]
         elif img2_width > img1_width:
             min_value = img1_width
-            cropped_img2 = img2[,0:0 + min_value]
-
+            cropped_img2 = img2[0:0+img2_height,0:0 + min_value]
 
     else:
-        print("Error")
+        logging.exception("Error occurred cropping images ...")
 
     return cropped_img1, cropped_img2
+
 
 
 def show_img(window_name, img):
