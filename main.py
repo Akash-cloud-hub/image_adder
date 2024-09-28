31b470d9b6fda5a6b9de412853d524736b8bf647
Akash <airshula79@gmail.com>
2024-09-28 21:54:39 +0700

    first commit

--- main.py
+++ main.py
@@ -13,38 +13,42 @@ import logging
 
 
 def main():
-    img1 = cv2.imread('moon1_cropped.jpg')
+    img1 = cv2.imread('sun-man_cropped.jpg')
     img2 = cv2.imread('monkey-nft.png')
+    img3 = cv2.imread('porche911GT3R.jpg')
+    img4=cv2.imread('moon1_cropped.jpg')
     # v_combined_img = combine_vertically(img1, img2)
     # show_img("vertical", v_combined_img)
 
     height = img1.shape[0]
     width = img1.shape[1]
 
-    img1 = cv2.resize(img1 , (width//2,height//2)).copy()
-    img2 = cv2.resize(img2 , (width//2,height//2)).copy()
+    beta = 0.5
+    overlay_img = overlay(img1,img2,beta)
+    overlay_img2 = overlay(img3,img4,beta)
+
+    # overlay_img , overlay_img2 = crop_img(overlay_img , overlay_img2 , )
+    v_combined_img = combine_vertically(overlay_img,overlay_img2)
+
+    show_img("overlayed_img",v_combined_img)
+
+def overlay(img1,img2,beta):
+    height = img1.shape[0]
+    width = img1.shape[1]
+    img1 = cv2.resize(img1, (width // 2, height // 2)).copy()
+    img2 = cv2.resize(img2, (width // 2, height // 2)).copy()
     beta = 0.5
     overlay_img = cv2.addWeighted(img1, 1 - beta, img2, beta, 0)
-
-    show_img("overlayed_img",overlay_img)
-
-
+    return overlay_img
 
 def combine_vertically(img1, img2):
     if img1 is None or img2 is None:
         print("unable to load images")
     else:
-
-        # img1 , img2 = crop_img(img1 , img2 , False)
-
-        # img1 = cv2.resize(img1 , (img1.shape[0]//4,img2.shape[0]//2))
-        # img2 = cv2.resize(img2 , (img2.shape[0]//2,img2.shape[0]//2))
-        # h_combined = cv2.hconcat([img1,img2])
-        # return h_combined
-
-        img1= cv2.resize(img1, (img1.shape[1] // 2, img1.shape[0] // 2)).copy()
-        img2 = cv2.resize(img2, (img1.shape[1] // 2, img2.shape[0] // 2)).copy()
-        # img1, img2 = crop_img(img1, img2, True)
+        img1_width = img1.shape[1]
+        img2_width = img2.shape[1]
+        img1= cv2.resize(img1, (img1_width // 2, img1.shape[0] // 2)).copy()
+        img2 = cv2.resize(img2, (img1_width // 2, img2.shape[0] // 2)).copy()
 
         print(img1.shape[1] ,img2.shape[1] )
         print(img1.shape[0] ,img2.shape[0] )
